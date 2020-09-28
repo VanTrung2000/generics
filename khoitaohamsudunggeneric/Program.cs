052f49429e1506b5075718faf2849bc896761ca6
VanTrung2000 <vantrungdeptrai2000@gmail.com>
2020-09-28 13:33:25 +0700

    Add project files.

---
+++ khoitaohamsudunggeneric/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace khoitaohamsudunggeneric
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Generic<int> ge = new Generic<int>();
+            ge.SoSanh<int>(2, 2);
+            ge.SoSanh<int>(2, 4);
+            ge.SoSanh<string>("hai", "bon");
+            ge.SoSanh<string>("hai", "hai");
+
+        }
+    }
+}
